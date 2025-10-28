e2bd1bdda61b4a500271d63949ee0aa03d422678
nastya3323 <gomes-96@mail.ru>
2025-10-28 21:26:32 +0400

    feat: implement isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -173,8 +173,10 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((elem) => {
+    return elem.length === arr[0].length;
+  });
 }
 
 /**
