6211e88bb6df788c4e25116d7bc97a195ff0cf96
nastya3323 <gomes-96@mail.ru>
2025-10-28 21:23:54 +0400

    feat: implement getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -145,8 +145,22 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length <= 0) {
+    return 0;
+  }
+
+  const sumArray = arr.reduce((sum, elem) => {
+    return sum + elem;
+  }, 0);
+
+  const result = sumArray / arr.length;
+
+  if (!Number.isInteger(result)) {
+    return +result.toFixed(2);
+  }
+
+  return result;
 }
 
 /**
