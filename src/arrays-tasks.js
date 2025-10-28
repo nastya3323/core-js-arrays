4f5c37e1138652cad9eadf15895b485c1d5e8d7f
nastya3323 <gomes-96@mail.ru>
2025-10-28 21:06:12 +0400

    feat: implement sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -39,8 +39,24 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1.map((item, index) => {
+      if (arr2[index]) {
+        return item + arr2[index];
+      }
+
+      return item;
+    });
+  }
+
+  return arr2.map((item, index) => {
+    if (arr1[index]) {
+      return item + arr1[index];
+    }
+
+    return item;
+  });
 }
 
 /**
