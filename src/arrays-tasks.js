9c05d5188f791723f9947eb804023df59e993aa7
nastya3323 <gomes-96@mail.ru>
2025-11-11 14:51:18 +0400

    feat: implement getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -485,8 +485,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (elem, index) => {
+    return Array.from({ length: n }, (item, idx) => {
+      return index === idx ? 1 : 0;
+    });
+  });
 }
 
 /**
