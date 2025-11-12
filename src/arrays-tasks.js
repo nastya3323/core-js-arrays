f556f1ca49d8080b69e0feb92b977d7ae36889f9
nastya3323 <gomes-96@mail.ru>
2025-11-12 15:07:28 +0400

    feat: implement getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -545,8 +545,12 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  const sorted = arr.sort((a, b) => {
+    return b - a;
+  });
+
+  return sorted.slice(0, n);
 }
 
 /**
