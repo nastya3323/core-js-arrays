3e3defe54cc3072fc08682507722b1efac24ccfa
nastya3323 <gomes-96@mail.ru>
2025-11-12 16:43:36 +0400

    feat: implement shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -619,8 +619,22 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const result = [];
+
+  let start;
+  let end;
+
+  if (n > 0) {
+    start = arr.slice(-n);
+    end = arr.slice(0, n + 1);
+  } else {
+    start = arr.slice(-n);
+    end = arr.slice(0, -n);
+  }
+  result.push(...start, ...end);
+
+  return result;
 }
 
 /**
